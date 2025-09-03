b87ead78b40bcdab2ba73aca80920bb869e3a891
JM Macapagal <34160255+jmdmacapagal@users.noreply.github.com>
2025-09-04 01:47:53 +0300

    shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Store",
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const Homepage = () => {
   return (
     <div>
-      <h1>Welcome to the Homepage</h1>
+      <h1>Welcome to the Homepage1</h1>
+      <Button>Click Me</Button>
     </div>
   );
 };
