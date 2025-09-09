6cba70c5561f42bbe4aedc37179e0daa57735f37
JM Macapagal <34160255+jmdmacapagal@users.noreply.github.com>
2025-09-10 00:38:23 +0300

    loading and 404 page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const Homepage = () => {
+  // await delay(2000);
   return (
     <div>
       <h1>Welcome to the Homepage</h1>
