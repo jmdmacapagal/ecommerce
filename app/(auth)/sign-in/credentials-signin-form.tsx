a28ac14fc8613409efb019adfed5a8028544a352
JM Macapagal <34160255+jmdmacapagal@users.noreply.github.com>
2025-11-16 23:20:50 +0300

    user sign in

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { signInDefaultValues } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+
+const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant="default" disabled={pending}>
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
+  return (
+    <form action={action}>
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="email" className="mb-1">
+            Email
+          </Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+
+        <div>
+          <Label htmlFor="password" className="mb-1">
+            Password
+          </Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+
+        <div>
+          <SignInButton />
+        </div>
+
+        {data && !data.success && (
+          <div className="text-destructive text-center">{data.message}</div>
+        )}
+
+        <div className="text-sm text-muted-foreground text-center">
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialsSignInForm;
--- lib/validators.ts
+++ lib/validators.ts
@@ -9,16 +9,19 @@ const currency = z
   );
 
 export const insertProductSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long"),
-  slug: z.string().min(3, "Slug must be at least 3 characters long"),
-  category: z.string().min(3, "Category must be at least 3 characters long"),
-  brand: z.string().min(3, "Brand must be at least 3 characters long"),
-  description: z
-    .string()
-    .min(10, "Description must be at least 10 characters long"),
+  name: z.string().min(3, "Name must be at least 3 characters"),
+  slug: z.string().min(3, "Slug must be at least 3 characters"),
+  category: z.string().min(3, "Category must be at least 3 characters"),
+  brand: z.string().min(3, "Brand must be at least 3 characters"),
+  description: z.string().min(10, "Description must be at least 10 characters"),
   stock: z.coerce.number(),
   images: z.array(z.string()).min(1, "At least one image is required"),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
+});
+
+export const signInFromSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
 });
