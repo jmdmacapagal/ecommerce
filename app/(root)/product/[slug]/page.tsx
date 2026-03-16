6ef27a9141ad5e7faa0dabf2ec9ad128916ec19f
JM Macapagal <34160255+jmdmacapagal@users.noreply.github.com>
2026-03-17 01:33:04 +0300

    cart button update

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -7,6 +7,8 @@ import AddToCart from "@/components/shared/product/add-to-cart";
 import { getProductBySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 
+import { getMyCart } from "@/lib/actions/cart.actions";
+
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
 }) => {
@@ -14,6 +16,8 @@ const ProductDetailsPage = async (props: {
   const product = await getProductBySlug(slug);
 
   if (!product) notFound();
+
+  const cart = await getMyCart();
 
   return (
     <section>
@@ -67,6 +71,7 @@ const ProductDetailsPage = async (props: {
                 {product.stock > 0 && (
                   <div className="flex-center">
                     <AddToCart
+                      cart={cart}
                       item={{
                         productId: product.id,
                         name: product.name,
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,14 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus } from "lucide-react";
-import { CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
+import { CartItem, Cart } from "@/types";
 
 import { toast } from "sonner";
 
-import { addItemToCart } from "@/lib/actions/cart.actions";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ item, cart }: { item: CartItem; cart?: Cart }) => {
   const router = useRouter();
 
   const handleAddToCart = async () => {
@@ -31,7 +31,36 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  const handleRemoveItemFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    toast.success(res.message);
+  };
+
+  //   check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleRemoveItemFromCart}
+      >
+        <Minus className="w-4 h4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="w-4 h4" />
+      </Button>
+    </div>
+  ) : (
     <Button type="button" className="w-full" onClick={handleAddToCart}>
       <Plus /> Add To Cart
     </Button>
